f9086a9df6507bacc12338e4ff60f46de0dc0823
dhutchen <49037118+dhutchen@users.noreply.github.com>
2019-03-28 18:42:00 -0400

    Update lottoscript.sh

--- lottoscript.sh
+++ lottoscript.sh
@@ -6,14 +6,14 @@
   awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' numbersonly.csv | sort -k 2 -n
   printf "Above are the winning numbers. How many times each has won is listed beside it."
         printf "\n"
-#Print most likely to win combination of numbers.
+#Print most chosen to win combination of numbers.
   printf "\nThis is the most chosen combination of winning numbers: "
   awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' numbersonly.csv | sort -k 2 -n | awk 'NR>=73&&NR<=77' | awk '{print $1}' | paste -d, -s
   printf "\n"
 #Print second through fourth most chosen combinations of numbers.
-  printf "Second most likely:  "
+  printf "Second most chosen:  "
   awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' numbersonly.csv | sort -k 2 -n | awk 'NR>=68&&NR<=72' | awk '{print $1}' | paste -d, -s
-  printf "Third most likely:  "
+  printf "Third most chosen:  "
 
   awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' numbersonly.csv | sort -k 2 -n | awk 'NR>=63&&NR<=67' | awk '{print $1}' | paste -d, -s
   printf "Fourth most chosen:  "
